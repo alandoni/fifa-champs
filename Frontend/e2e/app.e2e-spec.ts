1683393735d30ade1ca6dc4e56da456f1114b7cb
Alan <alan_doni@hotmail.com>
2017-01-26 20:31:49 -0200

    Adding angular index

---
+++ Frontend/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FifaChampsPage } from './app.po';
+
+describe('fifa-champs App', function() {
+  let page: FifaChampsPage;
+
+  beforeEach(() => {
+    page = new FifaChampsPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
