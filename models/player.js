5a1d680943524bfa2f06f3050248a6da7ddc6f5e
Alan <alan_doni@hotmail.com>
2017-01-27 20:15:00 -0200

    Test get/insert/update/delete of all models

--- models/player.js
+++ models/player.js
@@ -3,7 +3,8 @@ var Schema   = Mongoose.Schema;
 
 var playerSchema = new Schema({
 	nickname: String,
-	password: String
+	password: String,
+	picture: String
 });
 
 var Player = Mongoose.model('Player', playerSchema);
