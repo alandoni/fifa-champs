53e15ded1a10c62b78b177f912c37e0bd6aaca9d
Cristiano Faustino <cristiadu@gmail.com>
2017-01-31 00:18:40 -0200

    Including materialize-css and angular2-materialize

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {MaterializeDirective} from "angular2-materialize";
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CreatePlayerComponent } from './create-player/create-player.component';
@@ -11,8 +13,10 @@ import { LoginService } from './login.service';
 import { PlayerService } from './player.service';
 import { ApiRequestService } from './api-request.service';
 
+
 @NgModule({
   declarations: [
+    MaterializeDirective,
     AppComponent,
     LoginComponent,
     CreatePlayerComponent
