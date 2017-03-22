3c9d10da322205d91575e9373c6b33d1896ed285
Alan Quintiliano <aquintiliano@daitangroup.com>
2017-03-22 15:30:16 -0300

    Fix mongooseController uppercase

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5001));
 const http = app.listen(app.get('port'));
 
 const databaseAddress = 'mongodb://localhost/test';
-const MongooseController = require('./MongooseController');
+const MongooseController = require('./mongooseController');
 const mongo = new MongooseController(databaseAddress);
 
 console.log("Listening at port: " + app.get("port"));
