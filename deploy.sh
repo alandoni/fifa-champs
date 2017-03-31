dadb5b93613f8b5b3b4350c5ebd9e0dfef822670
Cristiano Faustino <cristiadu@gmail.com>
2017-03-31 06:02:07 -0300

    Adding deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 pushd Frontend
+npm install
 npm run build
 popd
 pushd Backend
 ln -sf ../Frontend/dist ./dist
-nohup npm start &
 popd
+git push heroku `git subtree split --prefix Backend production`:master --force
