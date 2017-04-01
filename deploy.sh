f0154f07a62e4a66a13cb6157caac0df8fa6b9ed
Cristiano Faustino <cristiadu@gmail.com>
2017-04-01 16:26:10 -0300

    Changing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ npm run build
 popd
 pushd Backend
 rm -r dist
-cp ../Frontend/dist dist/
+cp -r ../Frontend/dist dist/
 popd
 git subtree split --branch production --prefix Backend/
+git checkout production
 git push -f
 git push heroku production:master --force
