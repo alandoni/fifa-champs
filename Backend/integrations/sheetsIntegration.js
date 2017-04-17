9907adfa8af2649637fd6d76282472b974158bcd
Cristiano Faustino <cristiadu@gmail.com>
2017-04-17 00:40:01 -0300

    adding more logic into sheets integration

--- Backend/integrations/sheetsIntegration.js
+++ Backend/integrations/sheetsIntegration.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
-
 var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 var TOKEN_DIR = 'resources/';
 var TOKEN_PATH = TOKEN_DIR + 'credentials_sheets.json';
@@ -12,6 +11,22 @@ var SECRET_PATH = TOKEN_DIR + 'client_secret_sheets.json';
 
 var SHEETS_ID = '1QfszMcuwleISfdd63pmUWavtnH6LCXhY_tWhnHerJ4g';
 var sheetMonths = process.argv.slice(2);
+
+const databaseAddress = (process.env.DB_ADDR || "mongodb://localhost/test");
+const MongooseController = require('../mongooseController');
+const mongo = new MongooseController(databaseAddress);
+const ChampionshipController = require('../controllers/championshipController');
+const PlayerController = require('../controllers/playerController');
+const MatchController = require('../controllers/matchController');
+
+
+var championshipModel = require('../models/championship')
+var matchModel = require('../models/match')
+var playerModel = require('../models/player')
+
+const championshipController = new ChampionshipController(mongo);
+const playerController = new PlayerController(mongo);
+const matchController = new MatchController(mongo);
 
 // Load client secrets from a local file.
 fs.readFile(SECRET_PATH, function processClientSecrets(err, content) {
@@ -98,37 +113,112 @@ function storeToken(token) {
   console.log('Token stored to ' + TOKEN_PATH);
 }
 
+function fetchDatesFromResult(values, arrayMatches, index, yearToSave, monthToSave) {
+  // Getting the dates from all Matches and populating arrayMatches
+  var dayToSave = null;
+  var currentDateBeingSet = null;
+  for (var dateIndex = 0; dateIndex < values[index].length; dateIndex++) {
+    // Date wil be null if it's the same as previous one
+    if (values[index][dateIndex] != "") {
+      dayToSave = values[index][dateIndex].slice(0, 2);
+      currentDateBeingSet = new Date("20" + yearToSave, monthToSave - 1, dayToSave);
+    }
+    arrayMatches.push(new matchModel({ date: currentDateBeingSet }));
+  }
+}
+
+function fetchScoreFromResult(values, arrayMatches, index, lastMatchArray) {
+  var currentMatch = lastMatchArray;
+  if (values[index + 2].length == values[index + 3].length) {
+    for (var scoreIndex = 0; scoreIndex < values[index + 2].length; scoreIndex += 2) {
+      arrayMatches[currentMatch].team1score = values[index + 2][scoreIndex];
+      arrayMatches[currentMatch].team2score = values[index + 3][scoreIndex];
+      currentMatch++;
+    }
+  }
+  else
+    console.error("Number of scores don't match!");
+}
+
+function fetchPlayersFromResult(values, arrayMatches, index, lastMatchArray, nicknamesArray) {
+  var currentMatch = lastMatchArray;
+  if (values[index + 1].length == values[index + 4].length) {
+    for (var playersIndex = 0; playersIndex < values[index + 4].length; playersIndex += 2) {
+      arrayMatches[currentMatch].player1 = getPlayerByNickname(values[index + 1][playersIndex], nicknamesArray)._id;
+      arrayMatches[currentMatch].player2 = getPlayerByNickname(values[index + 1][playersIndex + 1], nicknamesArray)._id;
+      arrayMatches[currentMatch].player3 = getPlayerByNickname(values[index + 4][playersIndex], nicknamesArray)._id;
+      arrayMatches[currentMatch].player4 = getPlayerByNickname(values[index + 4][playersIndex + 1], nicknamesArray)._id;
+      currentMatch++;
+    }
+  }
+  else
+    console.error("Number of players don't match!");
+}
+
+function getPlayerByNickname(player, nicknamesArray) {
+  for (var index in nicknamesArray) {
+    if (nicknamesArray[index].nickname == player)
+      return nicknamesArray[index];
+  }
+  return false;
+}
+
 function getMatchesFromSpreadsheet(auth, monthSheets) {
   var sheets = google.sheets('v4');
   var monthToInclude;
-  
-  monthSheets.forEach(function(val) {
+
+  monthSheets.forEach(function (val) {
 
     // Getting the current month to be included (based on the Sheet name).
-    monthToInclude = val.match(/\d\d\/\d\d/g);
+    monthToInclude = String(val.match(/\d\d\/\d\d/g));
 
     // Scaping the values in order to get the proper sheet.
-    val = val.replace("/","%2F");
+    val = val.replace("/", "%2F");
 
     sheets.spreadsheets.values.get({
       auth: auth,
       spreadsheetId: SHEETS_ID,
-      range: "'"+val+"'!O4:AQ47",
+      range: "'" + val + "'!O4:AQ47",
       majorDimension: 'COLUMNS'
     }, function (err, response) {
       if (err) {
         console.log('The API returned an error: ' + err);
         return;
       }
-      
+
       if (response.values == undefined) {
         console.log('Sheet not Found: ' + val);
         return;
       }
 
-      console.log("GETTING MATCHES FROM MONTH: "+ monthToInclude);
-      if(response.values.length > 0) {
 
+      console.log("GETTING MATCHES FROM MONTH: " + monthToInclude);
+      if (response.values.length > 0) {
+        var nicknamesArray;
+        playerController.getAll().then(responsePlayer => {
+          nicknamesArray = responsePlayer;
+
+          // Creating championship
+          var yearToSave = monthToInclude.slice(3);
+          var monthToSave = monthToInclude.slice(0, 2);
+          /* championshipsClient.post(null, new championshipModel({
+             month: monthToSave,
+             year: yearToSave,
+             date: new Date(yearToSave, monthToSave, 31),
+             isCurrent: false
+           }));*/
+
+          var arrayMatches = new Array();
+          var lastMatchArray = 0;
+          // Getting blocks of data gotten from spreadsheet
+          for (var i = 0; i < response.values.length; i += 6) {
+            fetchDatesFromResult(response.values, arrayMatches, i, yearToSave, monthToSave);
+            fetchScoreFromResult(response.values, arrayMatches, i, lastMatchArray);
+            fetchPlayersFromResult(response.values, arrayMatches, i, lastMatchArray, nicknamesArray);
+            lastMatchArray = arrayMatches.length;
+          }
+          console.log(arrayMatches);
+        });
       }
     });
   });
