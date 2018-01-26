e42f9523a3e24a42208a40a93b45c945dc8cfae8
Alan <alan_doni@hotmail.com>
2018-01-26 11:10:35 -0200

    Uncommenting lines

--- api/server.js
+++ api/server.js
@@ -7,14 +7,14 @@ const routes = require('./routes');
 
 const app = express();
 const databaseAddress = (process.env.DB_ADDR || "mongodb://localhost/test");
-//const mongo = new MongooseController(databaseAddress);
+const mongo = new MongooseController(databaseAddress);
 
 app.set('port', (process.env.PORT || process.env.FIFA_CHAMPS_PORT || 5001));
 const http = app.listen(app.get('port'));
 
 console.log("Listening at port: " + app.get("port"));
 
-//routes.set(app, mongo);
+routes.set(app, mongo);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
