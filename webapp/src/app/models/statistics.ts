cadca4bdebe0478f8a094a9d48d097eae869f94c
Alan Quintiliano <alan@doni.com>
2018-02-01 13:12:50 -0200

    Remove code to create statistics from standing-table.component, it will be on backend

--- webapp/src/app/models/statistics.ts
+++ webapp/src/app/models/statistics.ts
@@ -1,16 +1,19 @@
+import { Player } from './player';
+import { Match } from './match';
+
 export class Statistics {
-	player;
-	matches;
-	victories;
-	ties;
-	defeats;
-	goals;
-	concededGoals;
-	goalBalance;
-	goalsPerMatch;
-	concededGoalsPerMatch;
-	score;
-	percent;
+	player : Player;
+	matches : number;
+	victories : number;
+	ties : number;
+	defeats : number;
+	goals : number;
+	concededGoals : number;
+	goalBalance : number;
+	goalsPerMatch : number;
+	concededGoalsPerMatch : number;
+	score : number;
+	percent : number;
 
 	constructor() {
 		this.matches = 0;
@@ -24,5 +27,130 @@ export class Statistics {
 		this.concededGoalsPerMatch = 0;
 		this.score = 0;
 		this.percent = 0;
+	}
+
+	static getStatisticsFromMatches(matches: Array<Match>, limit : number) : Array<Statistics> {
+		const players : Array<Player> = this._getPlayersFromMatches(matches);
+
+		let statistics = this._createStatistics(matches, players);
+
+		statistics.sort((statistic1, statistic2) => {
+			return this._compareStatistics(limit, statistic1, statistic2);
+		});
+
+		return statistics;
+	}
+
+	static _getPlayersFromMatches(matches : Array<Match>) : Array<Player> {
+		let players = [];
+		for (let matchIndex in matches) {
+			const match = matches[matchIndex];
+			this._addPlayerIfContainsInMatch(players, match.player1);
+			this._addPlayerIfContainsInMatch(players, match.player2);
+			this._addPlayerIfContainsInMatch(players, match.player3);
+			this._addPlayerIfContainsInMatch(players, match.player4);
+		}
+		return players;
+	}
+
+	static _addPlayerIfContainsInMatch(playersList, player) {
+		if (playersList.indexOf(player) < 0) {
+			playersList.push(player);
+		}
+	}
+
+	static _createStatistics(matches, players) : Array<Statistics> {
+		let statistics = [];
+
+		for (let player in players) {
+			let playerObj = players[player];
+
+			if (!playerObj.nickname) {
+				continue;
+			}
+
+			statistics[playerObj.nickname] = new Statistics();
+			statistics[playerObj.nickname].player = playerObj;
+		}
+
+		let matchWihtoutFinal : Array<Match>;
+
+		if (Array.isArray(matches)) {
+			matchWihtoutFinal = matches.filter(match => match.isFinal == false);
+		} else {
+			matchWihtoutFinal = new Array<Match>();
+		}
+
+		for (let match in matchWihtoutFinal) {
+			let m = matchWihtoutFinal[match];
+			this._setStatisticOfPlayer(statistics, m.player1, m.team1score, m.team2score);
+			this._setStatisticOfPlayer(statistics, m.player2, m.team1score, m.team2score);
+			this._setStatisticOfPlayer(statistics, m.player3, m.team2score, m.team1score);
+			this._setStatisticOfPlayer(statistics, m.player4, m.team2score, m.team1score);
+		}
+
+		return statistics;
+	}
+
+	static _compareStatistics(limit, statistic1, statistic2) {
+		if (statistic1.matches >= limit && statistic2.matches < limit) {
+			return -1;
+		}
+		if (statistic1.matches < limit && statistic2.matches >= limit) {
+			return 1;
+		}
+
+		if (statistic1.percent > statistic2.percent) {
+			return -1;
+		}
+		if (statistic1.percent < statistic2.percent) {
+			return 1;
+		}
+
+		if ((statistic1.victories / statistic1.matches) > (statistic2.victories / statistic2.matches)) {
+			return -1;
+		}
+		if ((statistic1.victories / statistic1.matches) < (statistic2.victories / statistic2.matches)) {
+			return 1;
+		}
+
+		if ((statistic1.goalBalance / statistic1.matches) > (statistic2.goalBalance / statistic2.matches)) {
+			return -1;
+		}
+		if ((statistic1.goalBalance / statistic1.matches) < (statistic2.goalBalance / statistic2.matches)) {
+			return 1;
+		}
+
+		if (statistic1.goalsPerMatch > statistic2.goalsPerMatch) {
+			return -1;
+		}
+		if (statistic1.goalsPerMatch < statistic2.goalsPerMatch) {
+			return 1;
+		}
+		return 0;
+	}
+
+	static _setStatisticOfPlayer(statistics, player, team1score, team2score) {
+		if (!player.nickname) {
+			return;
+		}
+		statistics[player.nickname].player = player;
+		statistics[player.nickname].matches++;
+		statistics[player.nickname].goals += team1score;
+		statistics[player.nickname].concededGoals += team2score;
+		statistics[player.nickname].goalBalance += team1score - team2score;
+
+		if (team1score > team2score) {
+			statistics[player.nickname].victories++;
+			statistics[player.nickname].score += 3;
+		} else if (team1score < team2score) {
+			statistics[player.nickname].defeats++;
+		} else {
+			statistics[player.nickname].ties++;
+			statistics[player.nickname].score += 1;
+		}
+		statistics[player.nickname].goalsPerMatch = statistics[player.nickname].goals*1.0 / statistics[player.nickname].matches;
+		statistics[player.nickname].concededGoalsPerMatch = statistics[player.nickname].concededGoals*1.0 / statistics[player.nickname].matches;
+		statistics[player.nickname].percent = statistics[player.nickname].score / (3 * statistics[player.nickname].matches) * 100;
 	}
 }
--- webapp/src/app/standing-table/standing-table.component.ts
+++ webapp/src/app/standing-table/standing-table.component.ts
@@ -16,57 +16,32 @@ export class StandingTableComponent implements OnChanges {
 	@Input() noFilterSelected: Boolean;
 
 	statisticsList : Array<Statistics>;
-	matchesList : Array<Match>;
 	days = 0;
 	numberOfMatches = 0;
 	limit = 0;
-	players: Array<Player>;
 
 	constructor(private championshipService : ChampionshipService, private loginService : LoginService) { }
 
 	ngOnChanges(changes: SimpleChanges) {
 		this.setLimit();
-		this.players = this.getPlayersFromMatches();
 
-		this.statisticsList = [];
-		this.matchesList = [];
-
-		this.createStatistics();
+		this.statisticsList = Statistics.getStatisticsFromMatches(this.matches, this.limit);
 
 		// we should not update the stats it the statisticsList
 		// contains stats for all championships
-		if(!changes["noFilterSelected"]) {
+		if (!changes["noFilterSelected"]) {
 			this.updateStats();
 		}
 	}
 
-	getPlayersFromMatches() : Array<Player> {
-		let players = [];
-		for (let match in this.matches) {
-			if (players.indexOf(this.matches[match].player1) < 0) {
-				players.push(this.matches[match].player1);
-			}
-			if (players.indexOf(this.matches[match].player2) < 0) {
-				players.push(this.matches[match].player2);
-			}
-			if (players.indexOf(this.matches[match].player3) < 0) {
-				players.push(this.matches[match].player3);
-			}
-			if (players.indexOf(this.matches[match].player4) < 0) {
-				players.push(this.matches[match].player4);
-			}
-		}
-		return players;
-	}
-
 	updateStats() {
 		// update the stats only if the user is an admin
-		if(this.loginService.isLoggedIn()) {
-			if(this.statisticsList.length > 0) {
+		if (this.loginService.isLoggedIn()) {
+			if (this.statisticsList.length > 0) {
 				let champs = this.championshipService.getSelectedChampionship();
 				// we should not add stats for an unfinished championship
-				if(!champs.isCurrent){
-					champs.matches = this.matchesList;
+				if (!champs.isCurrent) {
+					champs.matches = this.matches;
 					champs.players = this.statisticsList.map(item => item.player);
 				} else {
 					// if the championship is unfinished, the stats must remain empty
@@ -76,114 +51,6 @@ export class StandingTableComponent implements OnChanges {
 				this.championshipService.update(champs._id, champs).subscribe((res) => {}, (err) => {});
 			}
 		}
-	}
-
-	createStatistics() {
-		if (this.statisticsList && this.statisticsList.length > 0) {
-			return;
-		}
-
-		let statistics = [];
-
-		if (!this.players) {
-			return;
-		}
-
-		for (let player in this.players) {
-			let playerObj = this.players[player];
-
-			if (!playerObj.nickname) {
-				continue;
-			}
-
-			statistics[playerObj.nickname] = new Statistics();
-			statistics[playerObj.nickname].player = playerObj;
-		}
-
-		let matchWihtoutFinal:Array<Match>;
-
-		if(Array.isArray(this.matches))
-			matchWihtoutFinal = this.matches.filter(match => match.isFinal == false);
-		else
-			matchWihtoutFinal = new Array<Match>();
-
-		for (let match in matchWihtoutFinal) {
-			let m = matchWihtoutFinal[match];
-			this.matchesList.push(m);
-			this.setStatisticOfPlayer(statistics, m.player1, m.team1score, m.team2score);
-			this.setStatisticOfPlayer(statistics, m.player2, m.team1score, m.team2score);
-			this.setStatisticOfPlayer(statistics, m.player3, m.team2score, m.team1score);
-			this.setStatisticOfPlayer(statistics, m.player4, m.team2score, m.team1score);
-		}
-
-		for (let key in statistics) {
-			this.statisticsList.push(statistics[key]);
-		}
-		this.statisticsList.sort((statistic1, statistic2) => {
-			return this.compareStatistics(statistic1, statistic2);
-		});
-	}
-
-	compareStatistics(statistic1, statistic2) {
-		if (statistic1.matches >= this.limit && statistic2.matches < this.limit) {
-			return -1;
-		}
-		if (statistic1.matches < this.limit && statistic2.matches >= this.limit) {
-			return 1;
-		}
-
-		if (statistic1.percent > statistic2.percent) {
-			return -1;
-		}
-		if (statistic1.percent < statistic2.percent) {
-			return 1;
-		}
-
-		if ((statistic1.victories / statistic1.matches) > (statistic2.victories / statistic2.matches)) {
-			return -1;
-		}
-		if ((statistic1.victories / statistic1.matches) < (statistic2.victories / statistic2.matches)) {
-			return 1;
-		}
-
-		if ((statistic1.goalBalance / statistic1.matches) > (statistic2.goalBalance / statistic2.matches)) {
-			return -1;
-		}
-		if ((statistic1.goalBalance / statistic1.matches) < (statistic2.goalBalance / statistic2.matches)) {
-			return 1;
-		}
-
-		if (statistic1.goalsPerMatch > statistic2.goalsPerMatch) {
-			return -1;
-		}
-		if (statistic1.goalsPerMatch < statistic2.goalsPerMatch) {
-			return 1;
-		}
-		return 0;
-	}
-
-	setStatisticOfPlayer(statistics, player, team1score, team2score) {
-		if (!player.nickname) {
-			return;
-		}
-		statistics[player.nickname].player = player;
-		statistics[player.nickname].matches++;
-		statistics[player.nickname].goals += team1score;
-		statistics[player.nickname].concededGoals += team2score;
-		statistics[player.nickname].goalBalance += team1score - team2score;
-
-		if (team1score > team2score) {
-			statistics[player.nickname].victories++;
-			statistics[player.nickname].score += 3;
-		} else if (team1score < team2score) {
-			statistics[player.nickname].defeats++;
-		} else {
-			statistics[player.nickname].ties++;
-			statistics[player.nickname].score += 1;
-		}
-		statistics[player.nickname].goalsPerMatch = statistics[player.nickname].goals*1.0 / statistics[player.nickname].matches;
-		statistics[player.nickname].concededGoalsPerMatch = statistics[player.nickname].concededGoals*1.0 / statistics[player.nickname].matches;
-		statistics[player.nickname].percent = statistics[player.nickname].score / (3 * statistics[player.nickname].matches) * 100;
 	}
 
 	setLimit() {
@@ -203,22 +70,21 @@ export class StandingTableComponent implements OnChanges {
 		this.days = days.length;
 		this.numberOfMatches = this.matches.length;
 
-		if(this.noFilterSelected){
+		if (this.noFilterSelected) {
 			this.limit = Math.floor(this.numberOfMatches / 15);
-		}
-		else{
+		} else {
 			this.limit = this.days;
 		}
 	}
 
 	hasPlayers() {
-		return this.players != null && this.players.length > 0;
+		return this.statisticsList != null && this.statisticsList.length > 0;
 	}
 
 	getSg(statistic){
-		if(statistic.goalBalance < 0)
+		if (statistic.goalBalance < 0) {
 			return 1;
-
+		}
 		return 0;
 	}
 
