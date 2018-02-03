a327d93bb243bc91961d1ce32a1e93c4dcd03b5c
Aléxis Kiosia <alexis.kiosia@gmail.com>
2018-02-03 17:54:26 -0200

    lint fixes to page-not-found files

--- webapp/src/app/page-not-found/page-not-found.component.spec.ts
+++ webapp/src/app/page-not-found/page-not-found.component.spec.ts
@@ -6,23 +6,23 @@ import { DebugElement } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 describe('PageNotFoundComponent', () => {
-  let component: PageNotFoundComponent;
-  let fixture: ComponentFixture<PageNotFoundComponent>;
+    let component : PageNotFoundComponent;
+    let fixture : ComponentFixture<PageNotFoundComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PageNotFoundComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ PageNotFoundComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PageNotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PageNotFoundComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
